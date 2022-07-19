6299393993b177eb1bfbeceb3f42ad932beaf5de
emuyundajnr26 <emuyundajnr@gmail.com>
2022-07-19 23:38:07 +0200

    css tune ups

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
 
+import React from 'react'
+import "./index.css"
 function App() {
   return (
     <div>
+    <h1>Login</h1>
+    <h3>Login into your account</h3>
       <form>
         <label htmlfor='id'>ID</label>
         <input type='text' id='id' name='ID'/>
         <br/>
         <label htmlfor='password'>Password</label>
         <input type='password' id='password' name='Password'/>
-        <button>Submit</button>
+        <br/>
+        <button>Login</button>
       </form>
     </div>
   );
